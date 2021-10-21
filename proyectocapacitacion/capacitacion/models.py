89bf1216df0f896630e60c884de6832beb993ba6
victor hugo navelinop <victornavelino@gmail.com>
2021-10-21 12:51:48 -0300

    modelo

--- proyectocapacitacion/capacitacion/models.py
+++ proyectocapacitacion/capacitacion/models.py
@@ -6,10 +6,10 @@ class Capacitacion(models.Model):
     titulo= models.CharField(max_length=250)
     objetivo = models.CharField(max_length=250)
     informacionAlPublico = models.CharField(max_length=250)
-    anio = models.IntegerField()
-    fechas = models.DateTimeField()
-    cantidadMinimaAsistencia = models.IntegerField()
-    cantidadHoras = models.IntegerField()
+    anio = models.IntegerField(blank=True, null=True)
+    fecha = models.DateTimeField(auto_now=True)
+    cantidadMinimaAsistencia = models.IntegerField(blank=True, null=True)
+    cantidadHoras = models.IntegerField(blank=True, null=True)
 
 
 def __str__(self):
